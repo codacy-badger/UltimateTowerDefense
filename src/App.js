a2c7ad1f456d9d3a3f1e3b749ceb07cc5bd3f651
Nikhil Bhaskar <nikhil.bhaskar@mountblue.io>
2018-08-06 18:49:13 +0530

    add game component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Game from './components/Game';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> ULTIMATE TOWER DEFENSE </h1>
+        <Game />
       </div>
     );
   }
